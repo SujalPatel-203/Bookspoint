537af2e66dd744d495fd493082014bc9d2fea44a
SujalPatel-203 <patelsujal025@gmail.com>
2025-02-16 10:37:39 +0530

    Your commit message here

--- src/pages/home/DisplayBooks.jsx
+++ src/pages/home/DisplayBooks.jsx
@@ -10,12 +10,12 @@ const DisplayBooks = () => {
 
   useEffect(() => {
     const categorizedBooks = categories.reduce((acc, category) => {
-      acc[category] = Books.filter(book => book.genre === category).slice(0,5);
+      acc[category] = Books.filter(book => book.genre === category).slice(0, 5);
       return acc;
-    },{});
+    }, {});
     setBooksByCategory(categorizedBooks);
   }, []);
-  
+
   return (
     <div id='books' className='max-w-6xl mx-auto mt-14'>
       {categories.map(category => (
@@ -24,8 +24,8 @@ const DisplayBooks = () => {
             <h1 className='text-xl'>{category}</h1>
             <Link to={`/categories/${category}`}>See All</Link>
           </div>
-          <div className="grid grid-cols-5 gap-y-4 justify-items-center max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 max-sm:grid-rows-2 max-xs:grid-cols-1">
-            <BookCard books={booksByCategory[category] || []}/>
+          <div className="grid grid-cols-1 gap-y-4 justify-items-center xs:grid-rows-2 xs:grid-cols-2 sm:grid-cols-3 sm:grid-rows-1 md:grid-cols-4 lg:grid-cols-5">
+            <BookCard books={booksByCategory[category] || []} />
           </div>
         </div>
       ))}
