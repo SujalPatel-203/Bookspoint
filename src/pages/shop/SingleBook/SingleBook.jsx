d7f92511b79b064bfd4533cdccf39728dedaf992
SujalPatel-203 <patelsujal025@gmail.com>
2025-02-16 09:44:35 +0530

    Your commit message here

--- src/pages/shop/SingleBook/SingleBook.jsx
+++ src/pages/shop/SingleBook/SingleBook.jsx
@@ -53,42 +53,44 @@ const SingleBook = () => {
 
   return (
     <>
-      <div className='bg-secondary h-52 flex justify-center items-center flex-col'>
-        <h2 className='font-Fraunces text-3xl mb-4'>Single Book</h2>
-        <div className='font-Quicksand'>
-          <span>
-            <Link to='/'>Home </Link>
-          </span>
-          <span> &gt; </span>
-          <span className='font-medium'> {book.title}</span>
+      <section>
+        <div className='bg-secondary h-52 flex justify-center items-center flex-col'>
+          <h2 className='font-Fraunces text-3xl mb-4'>Single Book</h2>
+          <div className='font-Quicksand'>
+            <span>
+              <Link to='/'>Home </Link>
+            </span>
+            <span> &gt; </span>
+            <span className='font-medium'> {book.title}</span>
+          </div>
         </div>
-      </div>
 
-      <div className='max-w-6xl mx-auto my-8 px-4 md:px-8'>
-        <div className='flex gap-8 max-md:flex-col'>
-          <div className='max-md:self-center'>
-            <img src={book.image} alt={book.title} className='h-auto w-auto rounded-md' loading='lazy' />
-          </div>
-          <div className='self-center'>
-            <h3 className='text-2xl font-Fraunces font-semibold mb-4'>{book.title}</h3>
-            <p className='text-xl font-Quicksand mb-4'>${book.price}</p>
-            <p className='font-Quicksand mb-4'>{book.description}</p>
-            <div className='font-Quicksand'>
-              <p><strong className='font-Fraunces'>Author :</strong> {book.author}</p>
-              <p><strong className='font-Fraunces'>Genre :</strong> {book.genre}</p>
+        <div className='max-w-6xl mx-auto my-8 px-4 md:px-8'>
+          <div className='flex gap-8 flex-col md:flex-row'>
+            <div className='max-md:self-center'>
+              <img src={book.image} alt={book.title} className='h-auto w-auto rounded-md' loading='lazy' />
             </div>
-            <button
-              className="bg-primary text-white font-semibold border-1 cursor-pointer rounded-md flex-1 text-md mt-4 px-4 py-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleAddToCart(book);
-              }}
-            >
-              Add To Cart
-            </button>
+            <div className='self-center'>
+              <h3 className='text-2xl font-Fraunces font-semibold mb-4'>{book.title}</h3>
+              <p className='text-xl font-Quicksand mb-4'>${book.price}</p>
+              <p className='font-Quicksand mb-4'>{book.description}</p>
+              <div className='font-Quicksand'>
+                <p><strong className='font-Fraunces'>Author :</strong> {book.author}</p>
+                <p><strong className='font-Fraunces'>Genre :</strong> {book.genre}</p>
+              </div>
+              <button
+                className="bg-primary text-white font-semibold border-1 cursor-pointer rounded-md flex-1 text-md mt-4 px-4 py-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleAddToCart(book);
+                }}
+              >
+                Add To Cart
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
